feb48599932619ad2a1d2e696af4db71cbfa0505
Parker Moore <parkrmoore@gmail.com>
2013-08-08 14:08:39 +0200

    Add leadership.php for easier portability/editing

---
+++ inc/leadership.php
@@ -0,0 +1,48 @@
+<table class="leadership">
+  <caption><h2>Leadership</h2></caption>
+  <thead>
+    <tr>
+      <th>Position</th>
+      <th>Name</th>
+      <th>NetID</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>President</td>
+      <td>Parker Moore '14</td>
+      <td>pjm336</td>
+    </tr>
+    <tr>
+      <td>Vice President</td>
+      <td>Zach Porges '13, MPS '14</td>
+      <td>zip2</td>
+    </tr>
+    <tr>
+      <td>Treasurer</td>
+      <td>Sam Tung</td>
+      <td>sat83</td>
+    </tr>
+    <tr>
+      <td>Corporate Relations Co-Chairs</td>
+      <td>
+        Josh Freeberg <br>
+        Matt Baron
+      </td>
+      <td>
+        jaf287 <br>
+        mlb338
+      </td>
+    </tr>
+    <tr>
+      <td>Social Chair</td>
+      <td>Susie Forbath</td>
+      <td>sf335</td>
+    </tr>
+    <tr>
+      <td>Communications Chair</td>
+      <td>Brian O'Connor</td>
+      <td>bjo29</td>
+    </tr>
+  </tbody>
+</table>
